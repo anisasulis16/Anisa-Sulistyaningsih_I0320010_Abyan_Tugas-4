8119e6554188e84fb02def906ad3c63f1c29e7a0
Anisa S <Anisa.Sulistyaningsih16@gmail.com>
2021-03-24 17:48:59 +0700

    soal 2

---
+++ I0320010_soal2_tugas4.py
@@ -0,0 +1,15 @@
+#Soal no 2 Operator Floor
+
+#Menginputkan dua bilangan bulat
+print("MASUKKAN BILANGAN BULAT")
+bilangan1 = float(input("Masukkan bilangan pertama : "))
+bilangan2 = float(input("Masukkan bilangan kedua : "))
+
+print(".........................")
+print("Bilangan Bulat = ", bilangan1, ",", bilangan2)
+
+#Memproses program
+hasil_program = bilangan1 // bilangan2
+
+#Menampilkan Hasil
+print("Jadi banyaknya bilangan kedua dibagi oleh bilangan pertama sebanyak ", bilangan1, "//", bilangan2, "=", hasil_program, "kali.")
