9bd3bd88343e2881f22eb2c6f2a5577f15387ec2
Anisa S <Anisa.Sulistyaningsih16@gmail.com>
2021-03-24 07:33:22 +0700

    exercise 4.6

---
+++ I0320010_exercise 4.6.py
@@ -0,0 +1,13 @@
+#pengecekan membership in
+
+#Strings
+
+needle = "lo"
+haystack = "Hello World"
+
+#Check
+if needle in haystack:
+    print(needle, "is present in the string", haystack)
+else:
+    print("Not found")
+
