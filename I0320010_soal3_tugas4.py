fcbfc5c7c584eaf31c5e9a185d36282791894a5c
Anisa S <Anisa.Sulistyaningsih16@gmail.com>
2021-03-24 17:51:14 +0700

    soal 3

---
+++ I0320010_soal3_tugas4.py
@@ -0,0 +1,21 @@
+#Soal no 3 Operator Comparison
+
+#berat Bagasi Pada Maskapai
+
+a = 50      #Berat maksimal bagasi di maskapai
+x = 110     #Kondisi Benda 1
+y = 49      #Kondisi Benda 2
+
+#memproses program berupa syarat berat maksimum yang diperbolehkan adalah a
+programpengecekan1 = x <= a
+programpengecekan2 = y <= a
+
+#menampilkan Hasil
+print('Hasil Bagasi X adalah', x, '<=', a, '=', programpengecekan1)
+print('Hasil Bagasi y adalah', y, '<=', a, '=', programpengecekan2)
+
+
+
+
+
+
